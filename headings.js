bc583079881d542c464f6fcf738d78b7923e2be9
Nicholas Hoyt <nhoyt@webui.com>
2015-03-23 15:03:30 -0500

    Styling updates congruent with landmarks

--- headings.js
+++ headings.js
@@ -1,34 +1,56 @@
 javascript: (function() {
   var targetList = [
-    {selector: "h1", color: "rgba(0, 0, 0, 0.2)",       label: "H1"},
-    {selector: "h2", color: "rgba(235, 255, 47, 0.2)",  label: "H2"},
-    {selector: "h3", color: "rgba(130, 0, 125, 0.2)",   label: "H3"},
-    {selector: "h4", color: "rgba(0, 0, 125, 0.2)",     label: "H4"},
-    {selector: "h5", color: "rgba(215, 116, 44, 0.2)",  label: "H5"},
-    {selector: "h6", color: "rgba(215, 40, 40, 0.2)",   label: "H6"}
+    {selector: "h1", color: "navy",   label: "H1"},
+    {selector: "h2", color: "olive",  label: "H2"},
+    {selector: "h3", color: "purple", label: "H3"},
+    {selector: "h4", color: "green",  label: "H4"},
+    {selector: "h5", color: "gray",   label: "H5"},
+    {selector: "h6", color: "brown",  label: "H6"}
   ];
 
   var className  = "a11yGfdXALm1";
+  var zIndex = 100000;
+
+  function getScrollOffsets() {
+    var t;
+
+    var xOffset = (typeof window.pageXOffset === "undefined") ?
+      (((t = document.documentElement) || (t = document.body.parentNode)) &&
+        typeof t.ScrollLeft == 'number' ? t : document.body).ScrollLeft :
+      window.pageXOffset;
+
+    var yOffset = (typeof window.pageYOffset === "undefined") ?
+      (((t = document.documentElement) || (t = document.body.parentNode)) &&
+        typeof t.ScrollTop == 'number' ? t : document.body).ScrollTop :
+      window.pageYOffset;
+
+    return { x: xOffset, y: yOffset };
+  }
 
   function createOverlay (tgt, rect) {
-    var innerStyle = "text-align: right; padding: 2px 4px";
+    var innerStyle = "float: right; background-color: " + tgt.color + "; padding: 2px 3px";
+    var scrollOffsets = getScrollOffsets();
     var node = document.createElement("div");
 
     node.setAttribute("class", className);
+
     node.style.position = "absolute";
     node.style.overflow = "hidden";
-    node.style.zIndex = "999999";
+    node.style.zIndex = zIndex;
 
-    node.style.left = rect.left + "px";
-    node.style.top = rect.top + "px";
+    node.startLeft = (rect.left + scrollOffsets.x) + "px";
+    node.startTop = (rect.top + scrollOffsets.y) + "px";
+
+    node.style.left = node.startLeft;
+    node.style.top = node.startTop;
     node.style.height = Math.max(rect.height, 27) + "px";
     node.style.width = rect.width + "px";
 
-    node.style.border = "1px solid black";
-    node.style.backgroundColor = tgt.color;
-    node.style.color = "black";
+    node.style.boxSizing = "border-box";
+    node.style.border = "3px solid " + tgt.color;
+    node.style.color = "white";
     node.style.fontFamily = "Arial, Helvetica, 'Liberation Sans', sans-serif";
-    node.style.fontSize = "18px";
+    node.style.fontSize = "16px";
 
     node.innerHTML = '<div style="' + innerStyle + '">' + tgt.label + '</div>';
     return node;
