cd27e3bca8810ee558382c6e2ba213aa8dd88f6d
Nicholas Hoyt <nhoyt@webui.com>
2015-04-02 11:45:56 -0500

    Use separate elements and CSS

--- message.js
+++ message.js
@@ -1,27 +1,35 @@
 javascript: (function (utils) {
 
-  var header = "Landmarks Message";
+  var header = "Landmarks";
   var message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
   function hideMessage () {
     if (window.msgOverlay) {
-      utils.deleteMsgOverlay(window.msgOverlay)
+      utils.deleteMsgOverlay(window.msgOverlay);
       delete(window.msgOverlay);
       window.messageState = false;
     }
   }
 
-  function showMessage (msgHeader, msgText) {
-    var p = '<p style="margin: 10px 0; padding: 0">';
-    var h2 = '<h2 style="margin: 10px 0 0 0; padding: 0">';
+  function showMessage (msgHeader, msgText, closeHandler) {
+    var button, h2, div;
 
     if (!window.msgOverlay) window.msgOverlay = utils.createMsgOverlay();
-    window.msgOverlay.innerHTML = h2 + msgHeader + "</h2>" + p + msgText + "</p>";
+    button = window.msgOverlay.firstChild;
+    button.onclick = closeHandler;
+
+    h2 = document.createElement("h2");
+    h2.innerHTML = msgHeader;
+    window.msgOverlay.appendChild(h2);
+
+    div = document.createElement("div");
+    div.innerHTML = msgText;
+    window.msgOverlay.appendChild(div);
   }
 
   window.accessibility = function (flag) {
     window.messageState = (typeof flag === "undefined") ? true : !flag;
-    if (!flag) showMessage(header, message);
+    if (!flag) showMessage(header, message, hideMessage);
     else hideMessage();
   };
   window.accessibility(window.messageState);
@@ -29,9 +37,5 @@ javascript: (function (utils) {
   window.onresize = function () {
     if (window.msgOverlay) utils.setBoxGeometry(window.msgOverlay);
   };
-
-  window.onclick = function () {
-    hideMessage();
-  }
 
 })(OAAUtils);
