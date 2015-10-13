2fa4e616708da6c286c4e76f689c1b57a846b6c4
Nicholas Hoyt <nhoyt@webui.com>
2015-10-13 14:15:05 -0500

    Alphabetized entry list

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ module.exports = {
   entry: {
     forms:     "./forms.js",
     headings:  "./headings.js",
+    images:    "./images.js",
     landmarks: "./landmarks.js",
-    lists:     "./lists.js",
-    images:    "./images.js"
+    lists:     "./lists.js"
   },
   output: {
     path: path.join(__dirname, "build"),
