28abc419ebce7385e40bb931c6b64a964e4e7d3c
Nicholas Hoyt <nhoyt@webui.com>
2015-10-14 08:23:08 -0500

    Formatting tweaks

--- landmarks.js
+++ landmarks.js
@@ -9,16 +9,6 @@ import { getAttributeValue, getAccessibleName } from './utils/accname';
 (function () {
 
   // Filter function called on a list of elements returned by selector
-  // 'header, [role="banner"]'. It returns true for the following
-  // conditions: (1) element IS NOT a header element; (2) element IS a
-  // header element AND IS NOT a descendant of article or section.
-  function isBanner (element) {
-    if (element.tagName.toLowerCase() !== 'header') return true;
-    if (!isDescendantOf(element, ['article', 'section'])) return true;
-    return false;
-  }
-
-  // Filter function called on a list of elements returned by selector
   // 'footer, [role="contentinfo"]'. It returns true for the following
   // conditions: (1) element IS NOT a footer element; (2) element IS a
   // footer element AND IS NOT a descendant of article or section.
@@ -28,14 +18,24 @@ import { getAttributeValue, getAccessibleName } from './utils/accname';
     return false;
   }
 
+  // Filter function called on a list of elements returned by selector
+  // 'header, [role="banner"]'. It returns true for the following
+  // conditions: (1) element IS NOT a header element; (2) element IS a
+  // header element AND IS NOT a descendant of article or section.
+  function isBanner (element) {
+    if (element.tagName.toLowerCase() !== 'header') return true;
+    if (!isDescendantOf(element, ['article', 'section'])) return true;
+    return false;
+  }
+
   let targetList = [
-    {selector: 'aside:not([role]), [role="complementary"]', color: "brown",  label: "complementary"},
-    {selector: 'footer, [role="contentinfo"]',              color: "olive",  label: "contentinfo", filter: isContentinfo},
-    {selector: '[role="application"]',                      color: "teal",   label: "application"},
-    {selector: 'nav, [role="navigation"]',                  color: "green",  label: "navigation"},
-    {selector: 'header, [role="banner"]',                   color: "gray",   label: "banner", filter: isBanner},
-    {selector: '[role="search"]',                           color: "purple", label: "search"},
-    {selector: 'main, [role="main"]',                       color: "navy",   label: "main"}
+    {selector: 'aside:not([role]), [role="complementary"]',           color: "brown",  label: "complementary"},
+    {selector: 'footer, [role="contentinfo"]', filter: isContentinfo, color: "olive",  label: "contentinfo"},
+    {selector: '[role="application"]',                                color: "teal",   label: "application"},
+    {selector: 'nav, [role="navigation"]',                            color: "green",  label: "navigation"},
+    {selector: 'header, [role="banner"]', filter: isBanner,           color: "gray",   label: "banner"},
+    {selector: '[role="search"]',                                     color: "purple", label: "search"},
+    {selector: 'main, [role="main"]',                                 color: "navy",   label: "main"}
   ];
 
   let selectors = targetList.map(function (tgt) {return '<li>' + tgt.selector + '</li>';}).join('');
