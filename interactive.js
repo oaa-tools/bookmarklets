60b3f3ab2162873794a4d3fd2775e48289a408ba
Nicholas Hoyt <nhoyt@webui.com>
2015-10-17 18:10:38 -0500

    Added meter, output, progress

--- interactive.js
+++ interactive.js
@@ -10,23 +10,28 @@ import { formatInfo } from './utils/utils';
 
 (function () {
   let targetList = [
-    {selector: "input",      color: "navy",    label: "input"},
-    {selector: "button",     color: "purple",  label: "button"},
-    {selector: "label",      color: "olive",   label: "label"},
-    {selector: "keygen",     color: "teal",    label: "keygen"},
-    {selector: "select",     color: "green",   label: "select"},
-    {selector: "textarea",   color: "brown",   label: "textarea"},
-    {selector: "audio",      color: "maroon",  label: "audio"},
-    {selector: "embed",      color: "gray",    label: "embed"},
-    {selector: "iframe",     color: "navy",    label: "iframe"},
-    {selector: "img",        color: "purple",  label: "img"},
-    {selector: "area",       color: "maroon",  label: "area"},
-    {selector: "object",     color: "teal",    label: "object"},
-    {selector: "svg",        color: "green",   label: "svg"},
-    {selector: "video",      color: "brown",   label: "video"},
-    {selector: "a",          color: "olive",   label: "a"},
-    {selector: "details",    color: "gray",    label: "details"},
-    {selector: "[tabindex]", color: "navy",    label: "tabindex"}
+    {selector: "input",      color: "navy",   label: "input"},
+    {selector: "button",     color: "olive",  label: "button"},
+    {selector: "label",      color: "purple", label: "label"},
+    {selector: "keygen",     color: "teal",   label: "keygen"},
+    {selector: "meter",      color: "maroon", label: "meter"},
+    {selector: "output",     color: "brown",  label: "output"},
+    {selector: "progress",   color: "gray",   label: "progress"},
+    {selector: "select",     color: "green",  label: "select"},
+    {selector: "textarea",   color: "navy",   label: "textarea"},
+
+    {selector: "audio",      color: "olive",  label: "audio"},
+    {selector: "embed",      color: "purple", label: "embed"},
+    {selector: "iframe",     color: "teal",   label: "iframe"},
+    {selector: "img",        color: "maroon", label: "img"},
+    {selector: "area",       color: "brown",  label: "area"},
+    {selector: "object",     color: "gray",   label: "object"},
+    {selector: "svg",        color: "green",  label: "svg"},
+    {selector: "video",      color: "navy",   label: "video"},
+
+    {selector: "a",          color: "olive",  label: "a"},
+    {selector: "details",    color: "purple", label: "details"},
+    {selector: "[tabindex]", color: "teal",   label: "tabindex"}
   ];
 
   let selectors = targetList.map(function (tgt) {return tgt.selector;}).join(', ');
