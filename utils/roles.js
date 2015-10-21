2da8cf66e6cc9660653dc4fe9cb1dbc8adaaf7c2
Nicholas Hoyt <nhoyt@webui.com>
2015-10-21 12:30:34 -0500

    Added more up-to-date info from document cited

--- utils/roles.js
+++ utils/roles.js
@@ -2,9 +2,38 @@
 *   roles.js
 */
 
-import { isDescendantOf } from './dom';
-import { getAttributeValue } from './namefrom';
+import { isDescendantOf, hasParentWithName } from './dom';
+import { getAttributeValue, hasEmptyAltText } from './namefrom';
 
+/*
+*   inListOfOptions: Determine whether element is a child of
+*   1. a select element
+*   2. an optgroup element that is a child of a select element
+*   3. a datalist element
+*/
+function inListOfOptions (element) {
+  let parent = element.parentElement,
+      parentName = parent.tagName.toLowerCase(),
+      parentOfParentName = parent.parentElement.tagName.toLowerCase();
+
+  if (parentName === 'select')
+    return true;
+
+  if (parentName === 'optgroup' && parentOfParentName === 'select')
+    return true;
+
+  if (parentName === 'datalist')
+    return true;
+
+  return false;
+}
+
+/*
+*   getAriaRole: Get the value of the role attribute, if it is present. If
+*   not specified, get the default role of element if it has one. Based on
+*   ARIA in HTML (https://specs.webplatform.org/html-aria/webspecs/master/)
+*   as of 21 October 2015.
+*/
 export function getAriaRole (element) {
   let tagName = element.tagName.toLowerCase(),
       type    = element.type;
@@ -13,77 +42,127 @@ export function getAriaRole (element) {
     return getAttributeValue(element, 'role');
 
   switch (tagName) {
+
     case 'a':
+      if (element.hasAttribute('href'))
+        return 'link';
+      break;
+
     case 'area':
-      return 'link';
-    case 'article':
-      return 'article';
-    case 'aside':
-      return 'complementary';
-    case 'body':
-      return 'document';
-    case 'button':
-      return 'button';
-    case 'datalist':
-      return 'listbox';
-    case 'fieldset':
-      return 'group';
+      if (element.hasAttribute('href'))
+        return 'link';
+      break;
+
+    case 'article':     return 'article';
+    case 'aside':       return 'complementary';
+    case 'body':        return 'document';
+    case 'button':      return 'button';
+    case 'datalist':    return 'listbox';
+    case 'details':     return 'group';
+    case 'dialog':      return 'dialog';
+    case 'dl':          return 'list';
+    case 'fieldset':    return 'group';
 
     case 'footer':
       if (!isDescendantOf(element, ['article', 'section']))
         return 'contentinfo';
       break;
 
+    case 'form':        return 'form';
+
+    case 'h1':          return 'heading';
+    case 'h2':          return 'heading';
+    case 'h3':          return 'heading';
+    case 'h4':          return 'heading';
+    case 'h5':          return 'heading';
+    case 'h6':          return 'heading';
+
     case 'header':
       if (!isDescendantOf(element, ['article', 'section']))
         return 'banner';
       break;
 
-    case 'hr':
-      return 'separator';
+    case 'hr':          return 'separator';
+
     case 'img':
-      return 'img';
+      if (!hasEmptyAltText(element))
+        return 'img';
+      break;
 
     case 'input':
-      if (type === 'button' || type === 'image')
-        return 'button';
-      if (type === 'checkbox')
-        return 'checkbox';
-      if (type === 'radio')
-        return 'radio';
+      if (type === 'button')    return 'button';
+      if (type === 'checkbox')  return 'checkbox';
+      if (type === 'email')     return (element.hasAttribute('list')) ? 'combobox' : 'textbox';
+      if (type === 'image')     return 'button';
+      if (type === 'number')    return 'spinbutton';
+      if (type === 'password')  return 'textbox';
+      if (type === 'radio')     return 'radio';
+      if (type === 'range')     return 'slider';
+      if (type === 'reset')     return 'button';
+      if (type === 'search')    return (element.hasAttribute('list')) ? 'combobox' : 'textbox';
+      if (type === 'submit')    return 'button';
+      if (type === 'tel')       return (element.hasAttribute('list')) ? 'combobox' : 'textbox';
+      if (type === 'text')      return (element.hasAttribute('list')) ? 'combobox' : 'textbox';
+      if (type === 'url')       return (element.hasAttribute('list')) ? 'combobox' : 'textbox';
       break;
 
     case 'li':
-      return 'listitem';
+      if (hasParentWithName(element, ['ol', 'ul']))
+        return 'listitem';
+      break;
+
     case 'link':
-      return 'link';
-    case 'main':
-      return 'main';
+      if (element.hasAttribute('href'))
+        return 'link';
+      break;
+
+    case 'main':      return 'main';
+
     case 'menu':
-      return 'toolbar';
-    case 'nav':
-      return 'navigation';
-    case 'ol':
-      return 'list';
+      if (type === 'toolbar')
+        return 'toolbar';
+      break;
+
+    case 'menuitem':
+      if (type === 'command')   return 'menuitem';
+      if (type === 'checkbox')  return 'menuitemcheckbox';
+      if (type === 'radio')     return 'menuitemradio';
+      break;
+
+    case 'meter':       return 'progressbar';
+    case 'nav':         return 'navigation';
+    case 'ol':          return 'list';
+
     case 'option':
-      return 'option';
-    case 'output':
-      return 'status';
-    case 'progress':
-      return 'progressbar';
-    case 'section':
-      return 'region';
-    case 'select':
-      return 'listbox';
-    case 'textarea':
-      return 'textbox';
-    case 'ul':
-      return 'list';
+      if (inListOfOptions(element))
+        return 'option';
+      break;
+
+    case 'output':      return 'status';
+    case 'progress':    return 'progressbar';
+    case 'section':     return 'region';
+    case 'select':      return 'listbox';
+    case 'summary':     return 'button';
+
+    case 'tbody':       return 'rowgroup';
+    case 'tfoot':       return 'rowgroup';
+    case 'thead':       return 'rowgroup';
+
+    case 'textarea':    return 'textbox';
+
+    // TODO: th can have role 'columnheader' or 'rowheader'
+    case 'th':          return 'columnheader';
+
+    case 'ul':          return 'list';
   }
 
   return null;
 }
 
+/*
+*   nameFromIncludesContents: Determine whether element has an ARIA role
+*   with a nameFrom property the includes 'contents'.
+*/
 export function nameFromIncludesContents (element) {
   let role = getAriaRole(element);
   if (role === null) return false;
